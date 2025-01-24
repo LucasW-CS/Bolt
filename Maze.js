3ee8b6f5f6d53f3bb5b3d665728cf8229918b066
LucasW-CS <lucas.alt1614@gmail.com>
2025-01-24 08:02:29 -0500

    Init Maze.js Program

---
+++ Maze.js
@@ -0,0 +1,83 @@
+
+//Team Members: Lincoln Hess & Lucas Winfrey
+//Date: 1.24.2025
+//Program: Maze
+//Hardware: Bolt #SB-B2F8
+
+//beginProgram
+async function startProgram() {
+
+//Start & Say Start
+//Bolt will say Start acrossed the LED's & Say it out loud
+//Driver: Lucas Winfrey
+//Navigator: Lincoln Hess
+
+
+
+
+//Green Checkpoint
+//LED's turn Green
+//Driver: Lincoln Hess
+//Navigator: Lucas Winfrey
+
+
+
+
+//Sound #1 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Lucas Winfrey
+//Navigator: Lincoln Hess
+
+
+
+
+//Red Checkpoint
+//LED's turn Red
+//Driver: Lincoln Hess
+//Navigator: Lucas Winfrey
+
+
+
+
+//Sound #2 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Lucas Winfrey
+//Navigator: Lincoln Hess
+
+
+
+
+//Purple Checkpoint
+//LED's turn Purple
+//Driver: Lincoln Hess
+//Navigator: Lucas Winfrey
+
+
+
+
+//Sound #3 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Lucas Winfrey
+//Navigator: Lincoln Hess
+
+
+
+
+//Blue Checkpoint
+//LED's turn Blue
+//Driver: Lincoln Hess
+//Navigator: Lucas Winfrey
+
+
+
+
+//Finished & Say Finished
+//Bolt will say Finished acrossed the LED's & Say it out loud
+//Driver: Lucas Winfrey
+//Navigator: Lincoln Hess
+
+
+
+
+//endProgram
+}
